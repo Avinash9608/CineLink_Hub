f7ad5aa1a4e1c2a0ad9d4972b54147de3bf8061f
Avinash Kumar <avinashmadhukar4@gmail.com>
2025-07-05 11:46:28 +0530

    push on github

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://cine-link-hub.vercel.app', // replace with your actual site URL
+    generateRobotsTxt: true, // (optional) generate robots.txt
+    sitemapSize: 5000,
+  };
+  
